f73d0276b8b590f02ab6571adeca9d958dd007ac
Cheng H. Lee <clee@anaconda.com>
2021-04-02 18:24:02 -0500

    Bump to version 4.16.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
 
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX \
+    --disable-static    \
+    --enable-shared     \
+    --disable-gtk-doc
+
 make -j $CPU_COUNT
-make check
+
 make install
+
+# NOTE: Not the usual way of usual things, but `make install` *must* precede
+# `make check`; if it doesn't, fuzzing tests may fail on OS X build systems due
+# to libtasn1.dylib not being found in `$PREFIX/lib`.  This is likely due SIP
+# breaking the mechanisms libtool uses to set the library search path of the
+# test executables; examples of another project running into such issues:
+#   - https://dev.gnupg.org/T5159#144621
+#   - https://dev.gnupg.org/T5024#139701
+make check || \
+    { find . -name 'test-*.log' -exec cat {} +; exit 1; }
+
+rm -f ${PREFIX}/share/man/*/asn1* ${PREFIX}/share/info/libtasn1*
